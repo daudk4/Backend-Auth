f14a78f3da17395a4f434ab1c1c7fcaa1d9bbc7a
Daud Khan <daudu6944@gmail.com>
2025-04-23 18:55:29 +0500

    Add: login functionality

--- app.js
+++ app.js
@@ -36,6 +36,25 @@ server.post('/create', (req, res) => {
     })
 })
 
+server.get('/login', async (req, res) => {
+    res.render('login')
+
+})
+server.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+    if (!user) return res.send('Invalid credentials!')
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+            const token = jwt.sign({ email }, 'secretKey')
+            res.cookie('token', token)
+            res.send("yes, you can login")
+        }
+        else res.send('Invalid credentials!')
+    })
+})
+
 server.get('/logout', (req, res) => {
     res.cookie('token', "")
     res.redirect('/')
